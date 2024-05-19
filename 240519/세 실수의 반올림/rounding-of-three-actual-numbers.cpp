8221755b592d246efc1fa5f9e5294cd93a17575a
도현 <shickcoong777@gmail.com>
2024-05-19 16:26:59 +0900

    Title: 세 실수의 반올림, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

--- 240519/세 실수의 반올림/rounding-of-three-actual-numbers.cpp
+++ 240519/세 실수의 반올림/rounding-of-three-actual-numbers.cpp
@@ -7,8 +7,8 @@ int main() {
     double a, b, c;
     cin >> a >> b >> c;
     
-    cout << setprecision(4) << a << endl;
-    cout << setprecision(5) << b << endl;
+    cout << setprecision(4) << a << "\n";
+    cout << setprecision(5) << b << "\n";
     cout << fixed << setprecision(3) << c;
     return 0;
 }
