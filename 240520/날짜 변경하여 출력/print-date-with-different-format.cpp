2cd1161534d7ded1f064996f50d4f3687798ce77
도현 <shickcoong777@gmail.com>
2024-05-20 18:22:30 +0900

    Title: 날짜 변경하여 출력, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240520/날짜 변경하여 출력/print-date-with-different-format.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a, b, c;
+    char k,q = '-';
+    cin >> a >> k >> b >> k >> c;
+    cout << b << q << c << q << a; 
+    return 0;
+}
