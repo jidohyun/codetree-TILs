c0397d33b41468bba9a399752d0923f69fc94205
도현 <shickcoong777@gmail.com>
2024-05-20 18:28:19 +0900

    Title: 전화번호 바꾸기, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240520/전화번호 바꾸기/change-phone-number.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a, b, u;
+    char k;
+    cin >> u >> k >> a >> k >> b;
+    cout << "010" << k << b << k << a;
+    return 0;
+}
