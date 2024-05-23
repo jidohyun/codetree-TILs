fcfa2369f3c2395db1b68c06812f6440b6681a67
도현 <shickcoong777@gmail.com>
2024-05-23 15:23:51 +0900

    Title: 합과 차의 나눗셈, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240523/합과 차의 나눗셈/divide-of-sum-and-sub.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a, b;
+    cin >> a >> b;
+    cout << fixed;
+    cout.precision(2);
+    cout << double(a + b) / (a - b);
+    return 0;
+}
