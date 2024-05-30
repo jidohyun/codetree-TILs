185ad90426ed356b0527d33be05ffdd579540517
도현 <shickcoong777@gmail.com>
2024-05-30 21:34:49 +0900

    Title: 숫자의 조건 여부, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240530/숫자의 조건 여부/number's-condition.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a;
+    if(a <= 133)
+    {
+        cout << 1;
+    }
+    else
+    {
+        cout << 0;
+    }
+    return 0;
+}
