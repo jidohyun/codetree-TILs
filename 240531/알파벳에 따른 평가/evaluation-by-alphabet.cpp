156c85513080096cf408d13b845fba51e612f3cf
도현 <shickcoong777@gmail.com>
2024-05-31 16:03:01 +0900

    Title: 알파벳에 따른 평가, Time: -1ms, Memory: -1MB, Status: Compile Error - CodeTree

---
+++ 240531/알파벳에 따른 평가/evaluation-by-alphabet.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+using namespace std
+
+int main() {
+    char a;
+    cin >> a;
+    if(a == S)
+    {
+        cout << "Superior";
+    }
+    else if(a == A)
+    {
+        cout << "Excellent";
+    }
+    else if(a == B)
+    {
+        cout << "Usually";
+    }
+    else if(a == D)
+    {
+        cout << "Effort";
+    }
+    else
+    {
+        cout << "Failure";
+    }
+    return 0;
+}
