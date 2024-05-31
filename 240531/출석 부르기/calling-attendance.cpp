773db8a360465878bd0f37bbb087df13e1cf3d47
도현 <shickcoong777@gmail.com>
2024-05-31 15:04:16 +0900

    Title: 출석 부르기, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240531/출석 부르기/calling-attendance.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a;
+    cin >> a;
+    if(a == 1)
+    {
+        cout << "john";
+    }
+    else if(a == 2)
+    {
+        cout << "Tom";
+    }
+    else if(a == 3)
+    {
+        cout << "Paul";
+    }
+    else
+    {
+        cout << "Vancancy";
+    }
+    return 0;
+}
