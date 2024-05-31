e11dadbacfaee947397a505983fc7199dea01dfe
도현 <shickcoong777@gmail.com>
2024-05-31 14:45:02 +0900

    Title: 살 수 있는 물건, Time: 10ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240531/살 수 있는 물건/things-able-to-buy.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n;
+    cin >> n;
+    if(n < 1000)
+    {
+        cout << "no";
+    }
+    else if(n >= 1000 && n < 4000)
+    {
+        cout << "book";
+    }
+    else
+    {
+        cout << "mask";
+    }
+    return 0;
+}
