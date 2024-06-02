600945762e15e76ccb54c4abddd60db606469cc0
도현 <shickcoong777@gmail.com>
2024-06-02 22:47:27 +0900

    Title: 두 번의 연산, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240602/두 번의 연산/two-operations.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a;
+    cin >> a;
+    if(a % 2 != 0)
+    {
+        a += 3;
+    }
+    if(a % 3 == 0)
+    {
+        a /= 3;
+    }
+    cout << a;
+    return 0;
+}
