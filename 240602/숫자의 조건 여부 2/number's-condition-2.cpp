2cb75cbff34bdda57dfbc0700c0e85cc2dbb76c4
도현 <shickcoong777@gmail.com>
2024-06-02 22:27:39 +0900

    Title: 숫자의 조건 여부 2, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240602/숫자의 조건 여부 2/number's-condition-2.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a;
+    cin >> a;
+    if(a == 5)
+    {
+        cout << 'A';
+    }
+    else if(a % 2 == 0)
+    {
+        cout << 'B';
+    }
+    return 0;
+}
