5e10203a4266a9e4e6d8585cd29330d47927dd02
도현 <shickcoong777@gmail.com>
2024-06-02 21:36:12 +0900

    Title: 최대 2번의 연산, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 240602/최대 2번의 연산/up-to-2-calculations.cpp
+++ 240602/최대 2번의 연산/up-to-2-calculations.cpp
@@ -4,7 +4,14 @@ using namespace std;
 int main() {
     int a;
     cin >> a;
-    a % 2 == 0 ? a /= 2 : a = (a + 1) / 2;
+    if(a % 2 == 0)
+    {
+        a /= 2;
+    }
+    if(a % 2 != 0)
+    {
+        a = (a + 1) / 2;
+    }
     cout << a;
     return 0;
 }
